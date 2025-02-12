8c51c0f8ead74a5681d05864a5361f75de6156e6
MarioR <mariodev1050@gmail.com>
2025-02-12 16:58:29 -0600

    refactor: Se mejoro la forma de busqueda y validacion de datos en los servicios internos

--- src/main/java/com/proyectoUno/service/Internal/implementaciones/LibroServiceInternalImpl.java
+++ src/main/java/com/proyectoUno/service/Internal/implementaciones/LibroServiceInternalImpl.java
@@ -1,47 +1,161 @@
 package com.proyectoUno.service.Internal.implementaciones;
 
-import com.proyectoUno.dto.reponse.LibroResponseDTO;
-import com.proyectoUno.dto.request.libro.LibroActualizarRequestDTO;
 import com.proyectoUno.entity.Libro;
 import com.proyectoUno.exception.EntidadNoEncontradaException;
 import com.proyectoUno.repository.LibroRepository;
 import com.proyectoUno.service.Internal.interfaces.LibroServiceInternal;
+import com.proyectoUno.service.validation.interfaces.LibroValidacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class LibroServiceInternalImpl implements LibroServiceInternal {
 
     private LibroRepository libroRepository;
+    private LibroValidacionService libroValidacionService;
 
     @Autowired
-    public LibroServiceInternalImpl(LibroRepository libroRepository){
+    public LibroServiceInternalImpl(LibroRepository libroRepository, LibroValidacionService libroValidacionService){
 
         this.libroRepository=libroRepository;
+        this.libroValidacionService=libroValidacionService;
     }
+
     @Override
-    public Libro encontrarLibroPorId(UUID theid) {
+    public List<Libro> encontrarLibros() {
 
-        //Verificamos que se encuentre la entidad dentro del Optional, si no esta tiramos excepcion
-        Libro libro = libroRepository.findById(theid)
-                .orElseThrow(() -> new EntidadNoEncontradaException("El libro con ID " + theid + " no ha sido encontrado"));
+        //Buscamos Libros en el repository
+        List<Libro> librosEncontrados= libroRepository.findAll();
 
+        //Llamamos la servicio de validaciones
+        libroValidacionService.validarListaDeLibrosNoVacia(librosEncontrados);
 
-        //Convertimos Libro a DTO
+        return librosEncontrados;
+    }
+
+    @Override
+    public Libro encontrarLibroPorId(UUID id) {
+
+        //Encontrar El Optional en la base de datos
+        Optional<Libro> libroOptional = libroRepository.findById(id);
+
+        //Validar si existe o no
+        Libro libro = libroValidacionService.validarLibroExistencia(libroOptional);
+
         return libro;
+    }
+
+    @Override
+    public void eliminarLibroPorId(UUID id) {
+
+        try {
+
+            libroRepository.deleteById(id);
+
+        } catch(EntidadNoEncontradaException e){
+
+            throw new EntidadNoEncontradaException("Libro no encontrado, no puede ser eliminado");
+        }
 
     }
 
     @Override
     @Transactional
-    public void actualizarLibro(Libro libro) {
+    public Libro actualizarLibro(UUID id, Libro datosActualizacion) {
 
-       libroRepository.save(libro);
+        //Obtenemos y validamos los campos de la entidad parcial (estos datos asignaremos a la entidad existente)
+        libroValidacionService.validarDatosActualizacion(datosActualizacion);
 
+        //Encontramos (buscamos) la entidad que si esta presente en la base de datos con todos sus campos
+        Libro libroExistente= encontrarLibroPorId(id);
+
+        //Actualizar la entidad existente con la entidad parcial
+        libroExistente.setTitulo(datosActualizacion.getTitulo());
+        libroExistente.setAutor(datosActualizacion.getAutor());
+        libroExistente.setCategoria(datosActualizacion.getCategoria());
+        libroExistente.setEstado(datosActualizacion.getEstado());
+
+
+        return libroExistente;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+
+        //Encontramos el Optional con el isbn
+        Optional<Libro> libroExistente = libroRepository.findLibroByIsbn(libro.getIsbn());
+
+        // Validamos que no exista un Libro con el mismo isbn
+        libroValidacionService.validarLibroNoDuplicado(libroExistente,libro.getIsbn());
+
+        //Guardamos el libro
+        libroRepository.save(libro);
 
     }
 
+    @Override
+    public List<Libro> encontrarLibroPorTitulo(String titulo) {
+
+        //Encontrar la lista de libros
+        List <Libro> libros = libroRepository.findLibroByTituloContaining(titulo);
+
+        //Validar la lista
+        libroValidacionService.validarListaDeLibrosNoVacia(libros);
+
+        return libros;
+    }
+
+    @Override
+    public List<Libro> encontrarLibroPorAutor(String autor) {
+
+        //Encontrar la lista de libros
+        List <Libro> libros = libroRepository.findAllByAutorContaining(autor);
+
+        //Validar la lista
+        libroValidacionService.validarListaDeLibrosNoVacia(libros);
+
+        return libros;
+    }
+
+    @Override
+    public Libro encontrarLibroPorIsbn(String isbn) {
+
+        //Encontrar la lista de libros
+        Optional <Libro> libroOptional = libroRepository.findLibroByIsbn(isbn);
+
+        //verificacion que exista libro
+       Libro libro = libroValidacionService.validarLibroExistencia(libroOptional);
+
+        return libro;
+    }
+
+    @Override
+    public List<Libro> encontrarLibroPorCategoria(String categoria) {
+
+        //Encontrar la lista de libros
+        List <Libro> libros = libroRepository.findLibroByCategoriaContaining(categoria);
+
+        //Validamos la lista
+
+        libroValidacionService.validarListaDeLibrosNoVacia(libros);
+
+        return libros;
+
+    }
+    @Override
+    public List<Libro> encontrarLibroPorEstado(String estado) {
+
+        //Encontrar la lista de libros
+        List <Libro> libros = libroRepository.findByEstado(estado);
+
+        //Validamos la lista
+        libroValidacionService.validarListaDeLibrosNoVacia(libros);
+
+        return libros;
+    }
 }
--- src/main/java/com/proyectoUno/service/Internal/interfaces/LibroServiceInternal.java
+++ src/main/java/com/proyectoUno/service/Internal/interfaces/LibroServiceInternal.java
@@ -1,11 +1,35 @@
 package com.proyectoUno.service.Internal.interfaces;
 
+import com.proyectoUno.dto.reponse.LibroResponseDTO;
+import com.proyectoUno.dto.request.libro.LibroActualizarRequestDTO;
+import com.proyectoUno.dto.request.libro.LibroCrearRequestDTO;
 import com.proyectoUno.entity.Libro;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LibroServiceInternal {
 
-    Libro encontrarLibroPorId(UUID theid);
-    void actualizarLibro(Libro libro);
+    List<Libro> encontrarLibros ();
+
+    Libro encontrarLibroPorId (UUID theid);
+    void eliminarLibroPorId (UUID theid);
+
+    Libro actualizarLibro(UUID id , Libro libro);
+    void guardarLibro(Libro libro);
+
+    //metodos adicionales
+
+    List<Libro> encontrarLibroPorTitulo(String titulo);
+
+    List<Libro> encontrarLibroPorAutor(String autor);
+
+    Libro encontrarLibroPorIsbn(String isbn);
+
+    List<Libro> encontrarLibroPorCategoria(String categoria);
+
+    List<Libro> encontrarLibroPorEstado(String estado);
+
+
+
 }
