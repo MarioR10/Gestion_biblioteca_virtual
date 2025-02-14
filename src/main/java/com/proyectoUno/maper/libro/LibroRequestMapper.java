9a84a97bdcda50839e46a5d947349b7cb5f8f041
MarioR <mariodev1050@gmail.com>
2025-02-14 11:09:22 -0600

    Refactor: se refactorizo el maper, no se incluye en el mapeo la variable <estado> del libro

--- src/main/java/com/proyectoUno/maper/libro/LibroRequestMapper.java
+++ src/main/java/com/proyectoUno/maper/libro/LibroRequestMapper.java
@@ -15,7 +15,7 @@ public class LibroRequestMapper {
             libro.setTitulo(libroRequestDTO.getTitulo());
             libro.setAutor(libroRequestDTO.getAutor());
             libro.setCategoria(libroRequestDTO.getCategoria());
-            libro.setEstado(libroRequestDTO.getEstado());
+
 
         return libro;
 
