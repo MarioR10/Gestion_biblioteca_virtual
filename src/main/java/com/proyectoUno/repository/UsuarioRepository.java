bc79c0c680d9f77af1b1044b55b68c43c064ebd0
MarioR <mariodev1050@gmail.com>
2025-02-14 08:10:18 -0600

    feat: se implemento un nuevo metodo para encontrar usuarios por email

--- src/main/java/com/proyectoUno/repository/UsuarioRepository.java
+++ src/main/java/com/proyectoUno/repository/UsuarioRepository.java
@@ -3,9 +3,11 @@ package com.proyectoUno.repository;
 import com.proyectoUno.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional <Usuario> findByEmail(String email);
 
     //Ya vienen los CRUD basicos, si necesitamos metodos adicionales se agregan aqui;
 }
