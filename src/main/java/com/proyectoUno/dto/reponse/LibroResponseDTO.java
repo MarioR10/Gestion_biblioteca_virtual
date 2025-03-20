5d96f9e85d0edce9091b332846f39e3cc958c0af
MarioR <mariodev1050@gmail.com>
2025-03-20 13:07:21 -0600

    feat: se implementaron mas atributos para mostrar al cliente

--- src/main/java/com/proyectoUno/dto/reponse/LibroResponseDTO.java
+++ src/main/java/com/proyectoUno/dto/reponse/LibroResponseDTO.java
@@ -1,17 +1,28 @@
 package com.proyectoUno.dto.reponse;
 
+import java.util.UUID;
+
 public class LibroResponseDTO {
 
-    //Datos necesario a enviar al ususario
+    //Datos necesario a enviar
 
-
+    private UUID id;
     private String titulo;
     private String autor;
+    private String isbn;
+    private String categoria;
+    private Integer anioDePublicacion;
     private String estado;
 
 
 
     //Getter and Setter
+
+
+    public UUID getId() {return id;}
+
+    public void setId(UUID id) {this.id = id;}
+
     public String getTitulo() {
         return titulo;
     }
@@ -28,9 +39,19 @@ public class LibroResponseDTO {
         this.autor = autor;
     }
 
-    public String getEstado() {
-        return estado;
-    }
+    public String getIsbn() {return isbn; }
+
+    public void setIsbn(String isbn) {this.isbn = isbn; }
+
+    public String getCategoria() { return categoria;}
+
+    public void setCategoria(String categoria) {this.categoria = categoria;}
+
+    public Integer getAnioDePublicacion() {return anioDePublicacion; }
+
+    public void setAnioDePublicacion(Integer anioDePublicacion) {this.anioDePublicacion = anioDePublicacion;}
+
+    public String getEstado() {return estado;}
 
     public void setEstado(String estado) {
         this.estado = estado;
