52491b33869d09dd93baca426e6a22bf1b845ce3
MarioR <mariodev1050@gmail.com>
2025-03-24 09:23:44 -0600

    feat: se imlpementaron nuevos campos al DTO para actualizar el usuario

--- src/main/java/com/proyectoUno/dto/request/usuario/UsuarioActualizarDTO.java
+++ src/main/java/com/proyectoUno/dto/request/usuario/UsuarioActualizarDTO.java
@@ -1,17 +1,33 @@
 package com.proyectoUno.dto.request.usuario;
 
+import jakarta.persistence.Column;
+
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public class UsuarioActualizarDTO {
 
-    private UUID id;
+    @NotNull(message = "El nombre no puede ser nulo")
+    private String nombre;
+
+    @NotNull(message = "El apellido no puede ser nulo")
+    private String apellido;
 
     @NotNull(message = "El email no puede ser nulo")
     private String email;
 
     @NotNull(message = "El rol no puede ser nulo")
     private String rol;
+    @NotNull(message = "El estado dea ctividad no puede ser nulo")
+    private boolean activo;
+
+    public String getNombre() {return nombre;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getApellido() {return apellido;}
+
+    public void setApellido(String apellido) {this.apellido = apellido;}
 
     public String getEmail() {
         return email;
@@ -29,7 +45,7 @@ public class UsuarioActualizarDTO {
         this.rol = rol;
     }
 
-    public UUID getId() {
-        return id;
-    }
+    public boolean getActivo() {return activo;}
+
+    public void setActivo(boolean activo) {this.activo = activo;}
 }
