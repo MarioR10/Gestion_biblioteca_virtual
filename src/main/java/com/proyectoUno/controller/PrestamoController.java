c196ce9202ec9e0809fc5777b4a3f078fce6df65
MarioR <mariodev1050@gmail.com>
2025-03-26 09:41:26 -0600

    feat: se implementaron nuevos endpoints para la entidad Prestamo

--- src/main/java/com/proyectoUno/controller/PrestamoController.java
+++ src/main/java/com/proyectoUno/controller/PrestamoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/prestamo")
@@ -30,6 +31,38 @@ public class PrestamoController {
 
         prestamoServiceExternal.guardarPrestamo(request);
 
+    }
+
+    @GetMapping("/activos/{usuarioId}")
+    public ResponseEntity<List<PrestamoResponseDTO>> obtenerPrestamosActivosPorUsuario(@PathVariable UUID usuarioId){
+
+        List<PrestamoResponseDTO> prestamosActivos = prestamoServiceExternal.encontrarPrestamosActivosPorUsuarios(usuarioId);
+
+        return ResponseEntity.ok(prestamosActivos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PrestamoResponseDTO> obtenerPrestamoPorId(@PathVariable UUID id){
+
+
+        PrestamoResponseDTO prestamo = prestamoServiceExternal.encontrarPrestamoPorId(id);
+
+        return ResponseEntity.ok(prestamo);
+    }
+
+    @PutMapping("/devolucion/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void registrarDevolucion(@PathVariable UUID id){
+
+        prestamoServiceExternal.registrarDevolucion(id);
+    }
+
+    @GetMapping("/historial/{usuarioId}")
+    public ResponseEntity<List<PrestamoResponseDTO>> obtenerHistorialPrestamosPorUsuario(@PathVariable UUID usuarioId){
+
+       List<PrestamoResponseDTO> prestamos= prestamoServiceExternal.obtenerHistorialDePrestamoPorUsuario(usuarioId);
+
+       return ResponseEntity.ok(prestamos);
     }
 
 
