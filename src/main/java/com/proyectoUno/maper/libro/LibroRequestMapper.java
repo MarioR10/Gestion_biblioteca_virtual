fc644f61d4da20d52fb56640a5df7f351e66817b
MarioR <mariodev1050@gmail.com>
2025-03-31 08:41:28 -0600

    feat: se implemento un nuevo metodo para convertir una lista de DTOs a Entidad

--- src/main/java/com/proyectoUno/maper/libro/LibroRequestMapper.java
+++ src/main/java/com/proyectoUno/maper/libro/LibroRequestMapper.java
@@ -1,9 +1,13 @@
 package com.proyectoUno.maper.libro;
 
+import com.proyectoUno.dto.reponse.LibroResponseDTO;
 import com.proyectoUno.dto.request.libro.LibroActualizarRequestDTO;
 import com.proyectoUno.dto.request.libro.LibroCrearRequestDTO;
 import com.proyectoUno.entity.Libro;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class LibroRequestMapper {
@@ -44,5 +48,16 @@ public class LibroRequestMapper {
         return libro;
 
     }
+
+    //Metodo para convertir a lista de Entidades
+    // Convertir una lista de entidades en una lista de DTOs de respuesta
+    public List<Libro> convertirAListaEntidad(List<LibroCrearRequestDTO> libros){
+
+
+        return libros.stream()
+                .map(this::crearEntidadDesdeDTO)// Convierte cada Libro a un DTO (nuestro metodo anterior)
+                .collect(Collectors.toList()); //  Recolecta los resultados en una lista ahora de tipo DTO
+    }
+
 
 }
