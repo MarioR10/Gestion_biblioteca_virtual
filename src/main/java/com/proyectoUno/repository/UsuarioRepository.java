7224d82b6468ddbb48d1efaee7be5636d02e2ab0
MarioR <mariodev1050@gmail.com>
2025-03-31 10:56:35 -0600

    feat: se implemento un nuevo metodo que compara los correos nuevos con los existentes, si la comparacion es verdadera los guarda en una lista

--- src/main/java/com/proyectoUno/repository/UsuarioRepository.java
+++ src/main/java/com/proyectoUno/repository/UsuarioRepository.java
@@ -1,13 +1,18 @@
 package com.proyectoUno.repository;
 
+import com.proyectoUno.entity.Libro;
 import com.proyectoUno.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     Optional <Usuario> findByEmail(String email);
+
+    List <Usuario> findByEmailIn(List<String> emails);
+
 
     //Ya vienen los CRUD basicos, si necesitamos metodos adicionales se agregan aqui;
 }
