287de7a947c475e242d1cdc54b7f3c6911df4be4
Mario <mariodev1050@gmail.com>
2025-04-11 10:13:23 -0600

    feat: Se implementaron metodos que soportan paginacion en el repository para la la entidad Libro

--- src/main/java/com/proyectoUno/repository/LibroRepository.java
+++ src/main/java/com/proyectoUno/repository/LibroRepository.java
@@ -1,6 +1,8 @@
 package com.proyectoUno.repository;
 
 import com.proyectoUno.entity.Libro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,26 +11,25 @@ import java.util.UUID;
 
 public interface LibroRepository  extends JpaRepository<Libro, UUID> {
 
-    //Ya vienen los CRUD basicos, si necesitamos metodos adicionales se agregan aqui
-
-    // Containing indica que se va a hacer una búsqueda que podría devolver más de un libro cuyo título contenga la cadena que estamos  buscando
+    //metodosactuales sin paginacion
 
     // Encontrar por título (búsqueda parcial)
     List<Libro> findLibroByTituloContaining(String titulo);
-
     // Encontrar por autor (búsqueda parcial)
     List<Libro> findAllByAutorContaining(String autor);
-
     // Encontrar por ISBN
     List<Libro> findLibroByIsbn(String isbn);
-
-    //Encontrar por categoria
-
     // Encontrar por categoria (búsqueda parcial)
     List <Libro> findLibroByCategoriaContaining(String categoria);
-
     // Encontrar por estado
     List<Libro> findByEstado(String estado);
 
+    //metodos nuevos paginados
+
+    Page<Libro> findByTituloContaining(String titulo, Pageable pageable);
+    Page<Libro> findAllByAutorContaining(String autor, Pageable pageable);
+    Page<Libro> findLibroByIsbnContaining(String isbn, Pageable pageable);
+    Page<Libro> findLibroByCategoriaContaining(String categoria, Pageable pageable);
+    Page<Libro> findLibroByEstadoContaining(String estado, Pageable pageable);
    
 }
