c17d8fb2a6f32c5f4430cc99d0269fb605ea8c3e
MarioR <mariodev1050@gmail.com>
2025-04-16 11:27:08 -0600

    refact: se eliminaron metodos no paginados en el repositorio de la entidad libro

--- src/main/java/com/proyectoUno/repository/LibroRepository.java
+++ src/main/java/com/proyectoUno/repository/LibroRepository.java
@@ -11,21 +11,6 @@ import java.util.UUID;
 
 public interface LibroRepository  extends JpaRepository<Libro, UUID> {
 
-    //metodosactuales sin paginacion
-
-    // Encontrar por título (búsqueda parcial)
-    List<Libro> findLibroByTituloContaining(String titulo);
-    // Encontrar por autor (búsqueda parcial)
-    List<Libro> findAllByAutorContaining(String autor);
-    // Encontrar por ISBN
-    List<Libro> findLibroByIsbn(String isbn);
-    // Encontrar por categoria (búsqueda parcial)
-    List <Libro> findLibroByCategoriaContaining(String categoria);
-    // Encontrar por estado
-    List<Libro> findByEstado(String estado);
-
-    //metodos nuevos paginados
-
     Page<Libro> findByTituloContaining(String titulo, Pageable pageable);
     Page<Libro> findAllByAutorContaining(String autor, Pageable pageable);
     Page<Libro> findLibroByIsbnContaining(String isbn, Pageable pageable);
