cb55fe3a5c7028228c06de11140884884b6f859f
MarioR <mariodev1050@gmail.com>
2025-04-16 11:21:28 -0600

    feat: se implementaron DTOs de respuesta anidados al DTO de respuesta de prestamo, de esta forma se enriquece la informacion brindada al consultar uno o mas Prestamos

--- src/main/java/com/proyectoUno/dto/reponse/PrestamoResponseDTO.java
+++ src/main/java/com/proyectoUno/dto/reponse/PrestamoResponseDTO.java
@@ -1,38 +1,33 @@
 package com.proyectoUno.dto.reponse;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class PrestamoResponseDTO {
 
+    private UUID id;
     private LocalDateTime fechaPrestamo;
     private LocalDateTime fechaDevolucion;
     private String estado;
 
-
+    //DTOs anidados al DTO de prestamo (para enriquecer la informacion mostrada al mostrar el prestamo)
+    private UsuarioResponseDTO usuarioAsociado; // Anidado
+    private LibroResponseDTO libroAsociado; // Anidado
     //Getter and Setter
+    public LocalDateTime getFechaPrestamo() {return fechaPrestamo;}
+    public void setFechaPrestamo(LocalDateTime fechaPrestamo) {this.fechaPrestamo = fechaPrestamo;}
+    public LocalDateTime getFechaDevolucion() {return fechaDevolucion;}
+    public void setFechaDevolucion(LocalDateTime fechaDevolucion) {this.fechaDevolucion = fechaDevolucion;}
 
+    public String getEstado() {return estado;}
 
-    public LocalDateTime getFechaPrestamo() {
-        return fechaPrestamo;
-    }
+    public void setEstado(String estado) {this.estado = estado;}
 
-    public void setFechaPrestamo(LocalDateTime fechaPrestamo) {
-        this.fechaPrestamo = fechaPrestamo;
-    }
+    public UUID getId() {return id;}
 
-    public LocalDateTime getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(LocalDateTime fechaDevolucion) {
-        this.fechaDevolucion = fechaDevolucion;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+    public void setId(UUID id) {this.id = id;}
+    public UsuarioResponseDTO getUsuarioAsociado() {return usuarioAsociado;}
+    public void setUsuarioAsociado(UsuarioResponseDTO usuarioAsociado) {this.usuarioAsociado = usuarioAsociado;}
+    public LibroResponseDTO getLibroAsociado() {return libroAsociado;}
+    public void setLibroAsociado(LibroResponseDTO libroAsociado) {this.libroAsociado = libroAsociado;}
 }
