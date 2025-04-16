0765b9954f50e55bda52d4ba0bcb53cdd4d241a9
MarioR <mariodev1050@gmail.com>
2025-04-16 11:13:33 -0600

    feat: se implementan dos nuevos metodos con consulta personalizada para unir la tabla Prestamo con las tablas relacionadas Libro y Usuario, ADEMAS se elimina metodos no paginados

--- src/main/java/com/proyectoUno/repository/PrestamoRepository.java
+++ src/main/java/com/proyectoUno/repository/PrestamoRepository.java
@@ -5,27 +5,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, UUID> {
 
-
-    // Metodo para encontrar los préstamos activos por usuario
-    List<Prestamo> findPrestamoByUsuarioIdAndEstado(UUID theid, String estado);
-
-    // Metodo para encontrar el histórico de préstamos de un usuario
-    List<Prestamo> findByUsuarioId(UUID usuarioId);
-
-    // Metodo para encontrar préstamos activos
-    List<Prestamo> findByEstado(String estado);
-
-
-
-    //Metodos paginados
-    Page<Prestamo> findAll(Pageable pageable);
+    @Query("SELECT p FROM Prestamo p JOIN FETCH p.libro JOIN FETCH p.usuario WHERE p.id = :id")
+    Optional<Prestamo> findById(@Param( "id") UUID id);
     Page<Prestamo> findPrestamoByUsuarioIdAndEstado(UUID usuarioId, String estado, Pageable pageable);
     Page<Prestamo> findByUsuarioId( UUID id, Pageable pageable);
     Page<Prestamo> findByEstado( String estado, Pageable pageable);
+
+    @Query("SELECT p FROM Prestamo  p JOIN FETCH p.libro JOIN FETCH p.usuario")
+    Page<Prestamo> findAll(Pageable pageable);
 }
+
