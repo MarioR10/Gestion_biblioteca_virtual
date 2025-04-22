de01ecd68b6d25bbe878609566bf2ff5e87c9553
MarioR <mariodev1050@gmail.com>
2025-04-21 19:17:03 -0600

    feat: se implementaron interfaces qe son los nuevos mappers para la entidad Prestamo, utilizando la libreria MapperMapStruct

---
+++ src/main/java/com/proyectoUno/maper/prestamo/PrestamoResponseMapperStruct.java
@@ -0,0 +1,28 @@
+package com.proyectoUno.maper.prestamo;
+
+import com.proyectoUno.dto.reponse.LibroResponseDTO;
+import com.proyectoUno.dto.reponse.PrestamoResponseDTO;
+import com.proyectoUno.entity.Libro;
+import com.proyectoUno.entity.Prestamo;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface PrestamoResponseMapperStruct {
+
+    PrestamoResponseDTO toResponseDTO(Prestamo prestamo);
+    default Page<PrestamoResponseDTO> toResponseDTOPage(Page<Prestamo> prestamos){
+
+        List<PrestamoResponseDTO> dtoList = prestamos.getContent()
+                .stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(dtoList, prestamos.getPageable(), prestamos.getTotalElements());
+    }
+}
