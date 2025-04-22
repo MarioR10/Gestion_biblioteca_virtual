cc2793bc86c97282a4cb8a3a4b99482250b66beb
MarioR <mariodev1050@gmail.com>
2025-04-21 20:02:02 -0600

    feat: ahora las clases son record, lo que queda mejor para un DTO, se aplica este cambio tanto para la entidad Usuario como para Prestamo

--- src/main/java/com/proyectoUno/dto/reponse/UsuarioResponseDTO.java
+++ src/main/java/com/proyectoUno/dto/reponse/UsuarioResponseDTO.java
@@ -2,43 +2,14 @@ package com.proyectoUno.dto.reponse;
 
 import java.util.UUID;
 
-public class UsuarioResponseDTO {
+public record UsuarioResponseDTO(
 
-    private UUID id;
-    private String nombre;
-    private String apellido;
-    private String email;
-    private String rol;
+         UUID id,
+         String nombre,
+         String apellido,
+         String email,
+         String rol
 
-    //Metodo setter y getter
 
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getRol() {
-        return rol;
-    }
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
 
-    public UUID getId() {return id;}
-
-    public void setId(UUID id) {this.id = id;}
-
-}
+) {}
