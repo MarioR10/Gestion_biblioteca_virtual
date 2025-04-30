264504376f42cebc50797b5cc7954901308e5ad8
MarioR <mariodev1050@gmail.com>
2025-04-30 16:02:55 -0600

    feat: se implemento que el mapper autogenerado incluya los mappers de las otras entidades para convertir adecuadamente las entidades cargadas en Prestamo a sus correspondientes DTOs

--- src/main/java/com/proyectoUno/maper/prestamo/PrestamoResponseMapperStruct.java
+++ src/main/java/com/proyectoUno/maper/prestamo/PrestamoResponseMapperStruct.java
@@ -1,18 +1,19 @@
 package com.proyectoUno.maper.prestamo;
 
-import com.proyectoUno.dto.reponse.LibroResponseDTO;
+
 import com.proyectoUno.dto.reponse.PrestamoResponseDTO;
-import com.proyectoUno.entity.Libro;
 import com.proyectoUno.entity.Prestamo;
+import com.proyectoUno.maper.libro.LibroResponseMapperStruct;
+import com.proyectoUno.maper.usuario.UsuarioResponseMapperStruct;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+uses = {UsuarioResponseMapperStruct.class, LibroResponseMapperStruct.class})
 public interface PrestamoResponseMapperStruct {
 
     PrestamoResponseDTO toResponseDTO(Prestamo prestamo);
