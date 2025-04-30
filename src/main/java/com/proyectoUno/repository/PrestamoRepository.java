e10d2d2d10b41375351284d306e10fc3863fd01f
MarioR <mariodev1050@gmail.com>
2025-04-30 16:03:49 -0600

    feat: se implementaron querys personalizadas para que a cada consulta de Prestamo se le unan los datos necesarios de las otras entidades

--- src/main/java/com/proyectoUno/repository/PrestamoRepository.java
+++ src/main/java/com/proyectoUno/repository/PrestamoRepository.java
@@ -15,11 +15,16 @@ public interface PrestamoRepository extends JpaRepository<Prestamo, UUID> {
 
     @Query("SELECT p FROM Prestamo p JOIN FETCH p.libro JOIN FETCH p.usuario WHERE p.id = :id")
     Optional<Prestamo> findById(@Param( "id") UUID id);
-    Page<Prestamo> findPrestamoByUsuarioIdAndEstado(UUID usuarioId, String estado, Pageable pageable);
-    Page<Prestamo> findByUsuarioId( UUID id, Pageable pageable);
+
+    @Query("SELECT p FROM Prestamo p JOIN FETCH p.libro JOIN FETCH p.usuario WHERE p.usuario.id =:usuarioId AND p.estado =:estado")
+    Page<Prestamo> findPrestamoByUsuarioIdAndEstado(@Param("usuarioId") UUID usuarioId,@Param("estado") String estado, Pageable pageable);
+    @Query("SELECT p FROM Prestamo  p JOIN FETCH  p.libro JOIN FETCH p.usuario WHERE p.usuario.id =: usuarioId")
+    Page<Prestamo> findByUsuarioId(@Param("usuarioId") UUID id, Pageable pageable);
+
+    @Query( "SELECT  p FROM  Prestamo  p JOIN FETCH p.libro JOIN FETCH  p.usuario WHERE p.estado =: estado")
     Page<Prestamo> findByEstado( String estado, Pageable pageable);
 
-    @Query("SELECT p FROM Prestamo  p JOIN FETCH p.libro JOIN FETCH p.usuario")
+    @Query("SELECT p FROM Prestamo p JOIN FETCH p.libro JOIN FETCH p.usuario")
     Page<Prestamo> findAll(Pageable pageable);
 }
 
