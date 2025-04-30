1147ede19e0a3d7e2ca57f6c614daed20e0548b5
MarioR <mariodev1050@gmail.com>
2025-04-30 08:40:49 -0600

    feat: ahora los DTOs de la entidad Usuario son clases record, siguiendo las buenas practicas

--- src/main/java/com/proyectoUno/dto/request/usuario/UsuarioActualizarDTO.java
+++ src/main/java/com/proyectoUno/dto/request/usuario/UsuarioActualizarDTO.java
@@ -1,51 +1,12 @@
 package com.proyectoUno.dto.request.usuario;
 
-import jakarta.persistence.Column;
+public record UsuarioActualizarDTO(
 
-import javax.validation.constraints.NotNull;
-import java.util.UUID;
+        String nombre,
+        String apellido,
+        String email,
+        String rol,
+        boolean activo
+) {}
 
-public class UsuarioActualizarDTO {
 
-    @NotNull(message = "El nombre no puede ser nulo")
-    private String nombre;
-
-    @NotNull(message = "El apellido no puede ser nulo")
-    private String apellido;
-
-    @NotNull(message = "El email no puede ser nulo")
-    private String email;
-
-    @NotNull(message = "El rol no puede ser nulo")
-    private String rol;
-    @NotNull(message = "El estado dea ctividad no puede ser nulo")
-    private boolean activo;
-
-    public String getNombre() {return nombre;}
-
-    public void setNombre(String nombre) {this.nombre = nombre;}
-
-    public String getApellido() {return apellido;}
-
-    public void setApellido(String apellido) {this.apellido = apellido;}
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
-    public boolean getActivo() {return activo;}
-
-    public void setActivo(boolean activo) {this.activo = activo;}
-}
--- src/main/java/com/proyectoUno/dto/request/usuario/UsuarioCrearRequestDTO.java
+++ src/main/java/com/proyectoUno/dto/request/usuario/UsuarioCrearRequestDTO.java
@@ -1,61 +1,13 @@
 package com.proyectoUno.dto.request.usuario;
 
-import javax.validation.constraints.NotNull;
+public record UsuarioCrearRequestDTO(
 
-public class UsuarioCrearRequestDTO {
-
-
-    @NotNull(message = "El nombre no puede ser nulo")
-    private String nombre;
-    @NotNull(message = "El apellido no puede ser nulo")
-    private String apellido;
-    @NotNull(message = "El email no puede ser nulo")
-    private String email;
-    @NotNull(message = "La contraseña no puede ser nulo")
-    private String contraseña;
-    @NotNull(message = "El rol no puede ser nulo")
-    private String rol;
-
-    //metodos setter and getter
+        String nombre,
+        String apellido,
+        String email,
+        String contraseña,
+        String rol
 
 
-    public String getNombre() {
-        return nombre;
-    }
+) {}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-}
