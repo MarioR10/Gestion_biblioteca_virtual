0d452da7853ce166f5ca610149f9d67826bdf095
MarioR <mariodev1050@gmail.com>
2025-05-01 08:29:14 -0600

    feat: se implementan nuevas validaciones genericas, para utilizarse con cualquier entidad

--- src/main/java/com/proyectoUno/service/validation/interfaces/ValidacionService.java
+++ src/main/java/com/proyectoUno/service/validation/interfaces/ValidacionService.java
@@ -3,9 +3,11 @@ package com.proyectoUno.service.validation.interfaces;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ValidacionService {
 
     <T> void validarListaDeLibrosNoVacia(List<T> lista, String nombreEntidad);
     <T> void validarPaginaNoVacia(Page<T> pagina, String nombreEntidad);
+    <T> T  validarExistencia(Optional<T> entidad, String nombreEntidad);
 }
