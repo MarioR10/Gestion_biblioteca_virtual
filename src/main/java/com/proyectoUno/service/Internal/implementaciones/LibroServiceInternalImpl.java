4b1b036270370f1436f3ea6a4d44d07650eb3aea
MarioR10 <mariodev1050@gmail.com>
2025-06-11 10:51:50 -0600

    chore: se eliminan importaciones innecesarias en el servicio interno de la entidad Libro

--- src/main/java/com/proyectoUno/service/Internal/implementaciones/LibroServiceInternalImpl.java
+++ src/main/java/com/proyectoUno/service/Internal/implementaciones/LibroServiceInternalImpl.java
@@ -5,18 +5,13 @@ import com.proyectoUno.exception.EntidadNoEncontradaException;
 import com.proyectoUno.exception.SolicitudActualizacionInvalidaException;
 import com.proyectoUno.repository.LibroRepository;
 import com.proyectoUno.service.Internal.interfaces.LibroServiceInternal;
-import com.proyectoUno.service.validation.interfaces.LibroValidacionService;
-import com.proyectoUno.service.validation.interfaces.ValidacionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.ValidationException;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -24,14 +19,12 @@ import java.util.stream.Stream;
 public class LibroServiceInternalImpl implements LibroServiceInternal {
 
     private LibroRepository libroRepository;
-    private LibroValidacionService libroValidacionService;
-    private ValidacionService validacionService;
 
 
-    public LibroServiceInternalImpl(LibroRepository libroRepository, LibroValidacionService libroValidacionService, ValidacionService validacionService){
+
+    public LibroServiceInternalImpl(LibroRepository libroRepository){
         this.libroRepository=libroRepository;
-        this.libroValidacionService=libroValidacionService;
-        this.validacionService=validacionService;
+
     }
 
     //metodos actuales
