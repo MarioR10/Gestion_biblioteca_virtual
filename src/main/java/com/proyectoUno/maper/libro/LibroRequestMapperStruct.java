9b598f4c692baabebe957023034349ada346d43d
MarioR10 <mariodev1050@gmail.com>
2025-06-20 09:37:36 -0600

    feat: se implementaron nuevas anotaciones al mapper de actualizacion de entidades, la cual indica al mapper que debe ignorar los campos nulos del DTO, y la otra anotacion ayuda a pasarle una entidad existente para que no cree una nueva

--- src/main/java/com/proyectoUno/maper/libro/LibroRequestMapperStruct.java
+++ src/main/java/com/proyectoUno/maper/libro/LibroRequestMapperStruct.java
@@ -3,8 +3,7 @@ package com.proyectoUno.maper.libro;
 import com.proyectoUno.dto.request.libro.LibroActualizarRequestDTO;
 import com.proyectoUno.dto.request.libro.LibroCrearRequestDTO;
 import com.proyectoUno.entity.Libro;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
 
 
 import java.util.List;
@@ -13,8 +12,13 @@ import java.util.List;
 public interface LibroRequestMapperStruct {
 
     Libro toEntity(LibroCrearRequestDTO dto);
-    Libro  toEntity(LibroActualizarRequestDTO dto);
     List<Libro> toEntityList(List<LibroCrearRequestDTO> dtos);
+
+    //Metodo para mapear DTO -> Entidad existente
+    //La primera anotacion le indica a MapStruct que si un campo del DTO es null, no la incluya al mapeo de la entidad
+    //La segunda anotacion le indica a MapStruct que no cree una nueva instancia (Entidad), si no que se enfoque (Target) en la que le pasamos
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void  toUpdateEntity(LibroActualizarRequestDTO dto, @MappingTarget Libro libro);
 
 
 }
--- src/main/java/com/proyectoUno/maper/usuario/UsuarioRequestMapperStruct.java
+++ src/main/java/com/proyectoUno/maper/usuario/UsuarioRequestMapperStruct.java
@@ -3,15 +3,20 @@ package com.proyectoUno.maper.usuario;
 import com.proyectoUno.dto.request.usuario.UsuarioActualizarDTO;
 import com.proyectoUno.dto.request.usuario.UsuarioCrearRequestDTO;
 import com.proyectoUno.entity.Usuario;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import org.mapstruct.*;
+
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UsuarioRequestMapperStruct {
 
     Usuario toEntity(UsuarioCrearRequestDTO dto);
-    Usuario toEntity(UsuarioActualizarDTO dto);
     List<Usuario> toEntityList( List<UsuarioCrearRequestDTO> dtos);
+
+    //Metodo para mapear DTO -> Entidad existente
+    //La primera anotacion le indica a MapStruct que si un campo del DTO es null, no la incluya al mapeo de la entidad
+    //La segunda anotacion le indica a MapStruct que no cree una nueva instancia (Entidad), si no que se enfoque (Target) en la que le pasamos
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void toUpdateEntity(UsuarioActualizarDTO dto, @MappingTarget Usuario usuario);
 
 }
