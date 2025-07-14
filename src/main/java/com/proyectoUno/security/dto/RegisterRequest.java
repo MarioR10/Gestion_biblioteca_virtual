21159a582f178445613331a652714218e7a6544c
MarioR10 <mariodev1050@gmail.com>
2025-07-14 10:33:56 -0600

    refactor: se cambio el DTO de una clase tradicional a un record

--- src/main/java/com/proyectoUno/security/dto/RegisterRequest.java
+++ src/main/java/com/proyectoUno/security/dto/RegisterRequest.java
@@ -1,4 +1,10 @@
 package com.proyectoUno.security.dto;
 
-public class RegisterRequest {
+//DTO tener los datos necesarios para que un usuario se registre en nuestro sistema
+public record RegisterRequest(
+        String email,
+        String contrasena,
+        String nombre,
+        String apellido
+) {
 }
