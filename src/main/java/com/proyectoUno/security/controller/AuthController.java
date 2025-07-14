808e153bc0562ffbd8823fc7089f49dea97aea23
MarioR10 <mariodev1050@gmail.com>
2025-07-14 11:05:28 -0600

    feat: se implementaron los endpoint para iniciar sesion, registrarse u obtener un nuevo token de acceso

--- src/main/java/com/proyectoUno/security/controller/AuthController.java
+++ src/main/java/com/proyectoUno/security/controller/AuthController.java
@@ -1,4 +1,38 @@
 package com.proyectoUno.security.controller;
 
+import com.proyectoUno.security.dto.AuthResponse;
+import com.proyectoUno.security.dto.LoginRequest;
+import com.proyectoUno.security.dto.RegisterRequest;
+import com.proyectoUno.security.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController( AuthService authService){
+        this.authService=authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public  ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+
+        return  ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestParam String refreshToken){
+        return  ResponseEntity.ok(authService.refreshToken(refreshToken));
+    }
 }
