664bbc9355d6debabc03353696e91ad03881b1b3
MarioR10 <mariodev1050@gmail.com>
2025-07-14 11:06:03 -0600

    feat: se implemento una excepcion personalizada para los token invalidos

--- src/main/java/com/proyectoUno/exception/InvalidToken.java
+++ src/main/java/com/proyectoUno/exception/InvalidToken.java
@@ -1,7 +1,20 @@
 package com.proyectoUno.exception;
 
+import com.zaxxer.hikari.util.ClockSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class InvalidToken extends RuntimeException {
-  public InvalidToken(String message) {
+
+  //Loger que permite capturar errores/ excepciones en consola
+  private final Logger logger = LoggerFactory.getLogger(InvalidToken.class);
+
+  //ID de la excepcion
+  private final String internalCode = "INVALID_TOKEN";
+
+  public InvalidToken(String message){
+
     super(message);
   }
-}
+    }
+
