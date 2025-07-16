98b9b0dcd1a0e287c8e3db56bd767e1ac8f3ff51
MarioR10 <mariodev1050@gmail.com>
2025-07-16 10:32:15 -0600

    feat: se implemento la anotacion que permite anotar metodos restringidos por roles

--- src/main/java/com/proyectoUno/security/config/SecurityConfiguration.java
+++ src/main/java/com/proyectoUno/security/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import com.proyectoUno.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +28,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity // Habilita el soporte para @PreAuthorize
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
