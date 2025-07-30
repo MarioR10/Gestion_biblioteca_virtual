9fabf4c67a4610941beda8dab0c63f4e2cd703f1
MarioR10 <mariodev1050@gmail.com>
2025-07-30 14:48:54 -0600

    feat: se implementa un nuevo mapper para la entidad RefreshToken

--- src/main/java/com/proyectoUno/security/mapper/RefreshTokenMapper.java
+++ src/main/java/com/proyectoUno/security/mapper/RefreshTokenMapper.java
@@ -1,4 +1,23 @@
 package com.proyectoUno.security.mapper;
 
+import com.proyectoUno.entity.Usuario;
+import com.proyectoUno.security.entity.RefreshToken;
+import com.proyectoUno.security.jwt.JwtService;
+import org.mapstruct.*;
+
+import java.time.Instant;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RefreshTokenMapper {
+
+    @Mapping(target = "token", source = "refreshToken")
+    @Mapping(target = "revoked", constant= "false")
+    @Mapping(target = "usuario", source = "usuario")
+    @Mapping(target = "fechaExpiracion", qualifiedByName = "ObtenerFecha")
+    RefreshToken toEntity(String refreshToken, Usuario usuario, @Context JwtService jwtService);
+
+    @Named("ObtenerFecha")
+    default Instant fechaExpiracion ( @Context JwtService jwtService){
+        return Instant.now().plusMillis(jwtService.getEXPIRATION_REFRESH_TOKEN());
+    }
 }
