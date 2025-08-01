4841cd1ec282f9e74939e02bcdac4a8f65cb0f29
MarioR10 <mariodev1050@gmail.com>
2025-08-01 09:17:16 -0600

    feat: se implementan manejadores de excepciones para spring security y para la libreria io.jsonwebtoken

--- src/main/java/com/proyectoUno/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyectoUno/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.proyectoUno.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.MalformedKeyException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,11 +11,94 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> manejadorAutenticacionFallida( AuthenticationException ex, WebRequest request){
+
+        //Extraemos la URL de donde proviene la excepcion
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse= new ErrorResponse(
+                HttpStatus.UNAUTHORIZED,
+                ex.getMessage(),
+                path,
+                "AUTH_ERROR"
+        );
+        return  new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
+
+    }
+    @ExceptionHandler( AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> manejadorAccesoDenegado( AccessDeniedException ex, WebRequest request){
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.FORBIDDEN,
+                "Acceso denegado:" + ex.getMessage(),
+                path,
+                "ACCESS_DENIED"
+        );
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> manejadorJwtExpirado(ExpiredJwtException ex, WebRequest request){
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED,
+                ex.getMessage(),
+                path,
+                "TOKEN_EXPIRED"
+        );
+
+        return new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({SignatureException.class, MalformedKeyException.class, UnsupportedJwtException.class})
+    public ResponseEntity<ErrorResponse> manejadorJwtInvalido(Exception ex, WebRequest request){
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+
+                HttpStatus.UNAUTHORIZED,
+                "Token JWT inválido: " + ex.getMessage(),
+                path,
+                " INVALID_TOKEN"
+        );
+
+        return  new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> manejadorExcepcionesGenericas( Exception ex, WebRequest request){
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Ocurrio un error inesperado" + ex.getMessage(),
+                path,
+                "INTERNAL_ERROR"
+        );
+        return  new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+
+
 
     @ExceptionHandler(EntidadNoEncontradaException.class)
     public ResponseEntity<ErrorResponse> manejadorEntidadNoEncontrada(EntidadNoEncontradaException ex, WebRequest request){
