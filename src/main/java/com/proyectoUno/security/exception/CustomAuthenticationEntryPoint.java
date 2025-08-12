62ca4d8a98d930e306d06019b62ff2515c2c0f05
MarioR10 <mariodev1050@gmail.com>
2025-08-11 20:24:48 -0600

    feat: implementamos mecanismo para que se verifique primero si la solicitud HTTP trae consigo una excepcion lanzada por el filtro, antes de utilizar la excepcion predeterminada por Spring Security

--- src/main/java/com/proyectoUno/security/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/proyectoUno/security/exception/CustomAuthenticationEntryPoint.java
@@ -21,35 +21,53 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     private final Logger logger = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
     private final ObjectMapper objectMapper= new ObjectMapper().registerModule(new JavaTimeModule());
 
-/*
-El metodo recibe tres parametros importantes para su funcionamiento:
-    1. HttpServletRequest que representa la solicitud http que el cliente envio al servidor
-    2. HttpServletResponse representa la respuesta que nuestra aplicacion le mostrara al cliente.
-    3. es la excepcion que contiene los detalles del error de autenticacion que ocurrio.
- */
-@Override
-public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-    String path = request.getRequestURI();
-    logger.info("Entrando en CustomAuthenticationEntryPoint para path: {}, Excepción: {}", path, authException.getClass().getSimpleName()); // Depuración
-    logger.error("Error de Autenticación en {}: {}", path, authException.getMessage(), authException);
+    /*
+    El metodo recibe tres parametros importantes para su funcionamiento:
+        1. HttpServletRequest que representa la solicitud http que el cliente envio al servidor
+        2. HttpServletResponse representa la respuesta que nuestra aplicacion le mostrara al cliente.
+        3. es la excepcion que contiene los detalles del error de autenticacion que ocurrio.
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
-    ErrorResponse errorResponse = new ErrorResponse(
-            HttpStatus.UNAUTHORIZED,
-            authException.getMessage(),
-            path,
-            "AUTH_ERROR"
-    );
 
-    response.setContentType("application/json");
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    try (var writer = response.getWriter()) {
-        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
-        logger.info("Respondiendo con JSON para {}: {}", path, jsonResponse); // Depuración
-        writer.write(jsonResponse);
-        writer.flush(); // Asegura que se envíe la respuesta
-    } catch (IOException e) {
-        logger.error("Fallo al serializar o escribir la respuesta JSON: {}", e.getMessage(), e);
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error al procesar la respuesta");
+        //Obtenemos la excepcion que guardamos en el filtro
+        Exception exception = (Exception) request.getAttribute("excepcion");
+        String path = request.getRequestURI();
+        String errorMesage;
+
+        logger.info("Entrando en CustomAuthenticationEntryPoint para path: {}, Excepción: {}", path, exception.getClass().getSimpleName()); // Depuración
+        logger.error("Error de Autenticación en {}: {}", path, exception.getMessage(), exception);
+
+        if (exception != null){
+            logger.info("Manjeando excepcion lanzada por el filtro JWT: {}", exception.getClass().getSimpleName());
+            errorMesage = exception.getMessage();
+        } else {
+
+            //Si no hay ninguna excepcion guardada ocupamos la que Spring security nos da
+            logger.info("Manejando excepción de Spring Security: {}", authException.getClass().getSimpleName());
+            errorMesage = authException.getMessage();
+        }
+
+
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED,
+                errorMesage,
+                path,
+                "AUTH_ERROR"
+        );
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        try (var writer = response.getWriter()) {
+            String jsonResponse = objectMapper.writeValueAsString(errorResponse);
+            logger.info("Respondiendo con JSON para {}: {}", path, jsonResponse); // Depuración
+            writer.write(jsonResponse);
+            writer.flush(); // Asegura que se envíe la respuesta
+        } catch (IOException e) {
+            logger.error("Fallo al serializar o escribir la respuesta JSON: {}", e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error al procesar la respuesta");
+        }
     }
-}
 }
