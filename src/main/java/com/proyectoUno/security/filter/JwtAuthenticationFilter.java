6221017bf82e17174853f12331c8137f9961d739
MarioR10 <mariodev1050@gmail.com>
2025-08-11 20:29:28 -0600

    feat: se implemento un bloque try cath que envuelve la logica de verificacion en la lista redis y parseo del token para capturar las posibles excepciones, y de esta manera almacenarlas en la solitud HTTP y pasar al siguiente filtro de manera controlada

--- src/main/java/com/proyectoUno/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/proyectoUno/security/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.proyectoUno.security.filter;
 
-import com.proyectoUno.security.exception.RevokedTokenException;
+import com.proyectoUno.security.exception.JwtRevokedException;
 import com.proyectoUno.security.jwt.JwtService;
 import com.proyectoUno.security.service.TokenBlackListService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,6 +34,7 @@ public class JwtAuthenticationFilter  extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
     private final TokenBlackListService tokenBlackListService;
+    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     @Autowired
     public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService, TokenBlackListService tokenBlackListService){
@@ -52,16 +56,16 @@ public class JwtAuthenticationFilter  extends OncePerRequestFilter {
             //Representa la cadena de filtros de Spring Security, luego que este filtro haga su trabajo pasa la solciitud
             //al siguiente filtro en la cadena
             @NotNull FilterChain filterChain
-            ) throws ServletException, IOException{
+    ) throws ServletException, IOException {
 
         //1. definimos variables importantes
 
-            //Almacena el encabezado de la solicitud HTTP llamado "Authorization" ya que ahi suelen venir los JWT
-            //por ejemplo  Authorization: Bearer <el_jwt>
+        //Almacena el encabezado de la solicitud HTTP llamado "Authorization" ya que ahi suelen venir los JWT
+        //por ejemplo  Authorization: Bearer <el_jwt>
         final String authHeader = request.getHeader("Authorization");
-            //Almacena el JWT
+        //Almacena el JWT
         final String jwt;
-            //Almacena el UserName
+        //Almacena el UserName
         final String username;
 
         /*
@@ -72,63 +76,73 @@ public class JwtAuthenticationFilter  extends OncePerRequestFilter {
          Los encabezados de una solicitud Http son pares clave-valor (Authorization: Bearer <token>, u otro esquema como Basic
           en lugar de Bearer), nos interesa manejar autenticación basada en JWT */
 
-        if( authHeader ==null || !authHeader.startsWith("Bearer ")){
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             //Si alguna de las condiciones anteriores se cumple, signica que no hay un JWT que deba procesar,
             //pasa la solicitud al siguiente filtro(si lo hay) o directamente al controlador
-            filterChain.doFilter(request,response);
+            filterChain.doFilter(request, response);
             return; //detiene la eejcucion de este filtro, evitando que se proceda con la ejecucion
         }
 
         //3. Si el encabezado existe y comienza con el prefijo "Bearer", extraemos la cadena JWT.
         //substring(7) omite los primeros 7 caracteres "Bearer "  empieza directamente en el JWT indice (7) de la cadena
-        jwt= authHeader.substring(7); // "Bearrer " tiene 7 caracteres
+        jwt = authHeader.substring(7); // "Bearrer " tiene 7 caracteres
 
-        // --- VERIFICAR SI EL TOKEN ESTÁ EN LA LISTA NEGRA DE REDIS ---
+        try {
+            // --- VERIFICAR SI EL TOKEN ESTÁ EN LA LISTA NEGRA DE REDIS ---
 
-            if (tokenBlackListService.isTokenBlackListed(jwt)){
+            if (tokenBlackListService.isTokenBlackListed(jwt)) {
                 logger.info("Token marcado como revocado");
-                throw  new RevokedTokenException("El token fue revocado o invalido");
+                throw new JwtRevokedException("El token fue revocado o invalido");
 
             }
             //4. Extrae el username del JWT
-            username= jwtService.getUsernameFromToken(jwt);
+            username = jwtService.getUsernameFromToken(jwt);
 
-        //5. Aca verificamos dos cosas
+            //5. Aca verificamos dos cosas
             //5.1 Que se haya extraido exitosamente el subject del JWT
             /*5.2 Comprueba si no existe una autenticacion para la solicitud actual en el contexto de Spring Security
                 SecurityContextHolder es una clase donde se almacena la informacion del usuario autentificado (un Authetication),
                 devuelve un  objeto Authetication si esta autentificado o un null si no lo esta. Si  no es null significa
                 que otro mecanismo o filtro ya autentifico al usuario para esta solicitud y no es necesario volver a hacerlo*/
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication()==null){
-            //6. Carga los detalles del usuario desde la base de datos
-                UserDetails userDetails= this.userDetailsService.loadUserByUsername(username);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                //6. Carga los detalles del usuario desde la base de datos
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
-            //7.Verifica si el token es válido (no expirado, firma correcta, etc.) para este usuario cargado
-            if(jwtService.isTokenValid(jwt,userDetails)){
+                //7.Verifica si el token es válido (no expirado, firma correcta, etc.) para este usuario cargado
+                if (jwtService.isTokenValid(jwt, userDetails)) {
 
-                //8. Si el token es valido, crea un objeto de autentificacion (representa un usuario autentificado)
-                // UsernamePasswordAuthenticationToken es un objeto Authentication que indica que el usuario está autenticado
-                UsernamePasswordAuthenticationToken authToken= new UsernamePasswordAuthenticationToken(
-                        userDetails , //detalles del usuario
-                        null,  // La contraseña (ya no es necesaria porque ya se autenticó con el token)
-                        userDetails.getAuthorities()
-                );
+                    //8. Si el token es valido, crea un objeto de autentificacion (representa un usuario autentificado)
+                    // UsernamePasswordAuthenticationToken es un objeto Authentication que indica que el usuario está autenticado
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, //detalles del usuario
+                            null,  // La contraseña (ya no es necesaria porque ya se autenticó con el token)
+                            userDetails.getAuthorities()
+                    );
 
-                // 9. Establecer detalles de autenticación (opcional pero buena práctica)
-                // Esto añade detalles como la dirección IP y la sesión del usuario al objeto de autenticación
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
+                    // 9. Establecer detalles de autenticación (opcional pero buena práctica)
+                    // Esto añade detalles como la dirección IP y la sesión del usuario al objeto de autenticación
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
 
-                // 10. Actualizar el contexto de seguridad de Spring
-                // ¡Esto es lo más importante! Le dice a Spring Security que este usuario está autenticado
-                // para la solicitud actual. Ahora, Spring Security permitirá el acceso a recursos protegidos.
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                    // 10. Actualizar el contexto de seguridad de Spring
+                    // ¡Esto es lo más importante! Le dice a Spring Security que este usuario está autenticado
+                    // para la solicitud actual. Ahora, Spring Security permitirá el acceso a recursos protegidos.
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+            // 11. Pasar la solicitud al siguiente filtro en la cadena de seguridad
+            // Continúa con la ejecución de los demás filtros de Spring Security y, finalmente, al controlador
+            filterChain.doFilter(request, response);
+
+        }catch (JwtRevokedException | JwtException e) {
+            request.setAttribute("excepcion", e);
+            logger.error("Error de autenticación JWT: {}", e.getMessage());
+            filterChain.doFilter(request, response);
+        } catch (Exception e){
+            request.setAttribute("excepcion", e);
+            filterChain.doFilter(request, response);
         }
-        // 11. Pasar la solicitud al siguiente filtro en la cadena de seguridad
-        // Continúa con la ejecución de los demás filtros de Spring Security y, finalmente, al controlador
-        filterChain.doFilter(request, response);
     }
 }
