61e41a2563c7e68e9aa0dd7d3ebfa0036de78219
MarioR10 <mariodev1050@gmail.com>
2025-08-11 20:15:53 -0600

    style: Cambio en el mensaje de la excepcion que se lanza cuando existe un error inesperado en el servidor

--- src/main/java/com/proyectoUno/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proyectoUno/exception/GlobalExceptionHandler.java
@@ -6,13 +6,13 @@ import io.jsonwebtoken.security.MalformedKeyException;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.naming.AuthenticationException;
-import java.nio.file.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -88,7 +88,7 @@ public class GlobalExceptionHandler {
 
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR,
-                "Ocurrio un error inesperado" + ex.getMessage(),
+                "Ocurrio un error inesperado: " + ex.getMessage(),
                 path,
                 "INTERNAL_ERROR"
         );
